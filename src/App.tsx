e759bda602d3482294f8d21703d7a6512290de71
unknown <youssef93aitali@gmail.com>
2025-05-16 11:56:56 +0100

    Layouting welcome page - Adding Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+    <div className="flex flex-col min-h-svh">
+      <div className="flex justify-between items-center h-16 w-full px-[20%]">
+        <div>Logo</div>
+        <div className="flex gap-4">
+          <Button
+            variant="ghost"
+            className="hover:bg-gray-200/50 cursor-pointer"
+          >
+            Login
+          </Button>
+          <Button className="bg-cyan-900/90 hover:bg-cyan-900 cursor-pointer">
+            Sign up
+          </Button>
+        </div>
+      </div>
+      <div className="bg-gray-200 grow">Content</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
