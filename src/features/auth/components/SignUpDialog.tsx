9afaf18df2c3aee8d7fca6b89bd9fbc0cc34fed5
Youssef AIT ALI <youssef93aitali@gmail.com>
2025-05-26 20:32:08 +0100

    Adding ZOD to validate sign up form

--- src/features/auth/components/SignUpDialog.tsx
+++ src/features/auth/components/SignUpDialog.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { useNavigate } from "react-router";
 import { useForm, type SubmitHandler } from "react-hook-form";
@@ -18,7 +18,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 
@@ -29,6 +28,41 @@ import type {
   SignUpInputs,
 } from "@/features/auth/authTypes";
 import { useAuth } from "@/features/auth/hooks/useAuth";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import debounce from "lodash.debounce";
+
+const formSchema = z
+  .object({
+    username: z.string().min(1, {
+      message: "Username is required!",
+    }),
+    email: z
+      .string()
+      .min(1, {
+        message: "Email is required!",
+      })
+      .regex(/[A-Z]/, "Invalid email address!"),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters!",
+    }),
+    confirmPassword: z.string().min(1, {
+      message: "Confirm your password!",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match!",
+    path: ["confirmPassword"],
+  });
 
 const SignUpDialog = ({
   isSignUpDialogOpen,
@@ -36,6 +70,13 @@ const SignUpDialog = ({
   onLogInDialogOpenChange,
 }: SignUpDialogProps) => {
   const {
+    emailSignUp,
+    googleSignUp,
+    authLoading: { emailAuthLoading, googleAuthLoading },
+  } = useAuth();
+  const navigate = useNavigate();
+
+  /*   const {
     emailSignUp,
     googleSignUp,
     authLoading: { emailAuthLoading, googleAuthLoading },
@@ -85,7 +126,63 @@ const SignUpDialog = ({
     }
   };
 
-  const password = watch("password");
+  const password = watch("password"); */
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  // Then add debounce to the watch function
+  useEffect(() => {
+    const debounced = debounce(() => form.trigger("confirmPassword"), 500);
+    const subscription = form.watch(debounced);
+    return () => subscription.unsubscribe();
+  }, [form]);
+
+  const handleGoogleSignUp = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const result = await googleSignUp();
+    if (result.success) {
+      // Redirect or show success message
+      console.log("User created:", result.user);
+      onSignUpDialogOpenChange(false);
+      navigate("/dashboard");
+    } else {
+      toast.error(result.errorMessage);
+    }
+  };
+
+  // 2. Define a submit handler.
+  const onSubmit: SubmitHandler<SignUpInputs> = async ({
+    email,
+    password,
+    username,
+  }) => {
+    const result = await emailSignUp(email, password, username);
+    if (result.success) {
+      // Redirect or show success message
+      console.log("User created:", result.user);
+      onSignUpDialogOpenChange(false);
+      navigate("/dashboard");
+    } else {
+      toast.error(result.errorMessage);
+    }
+  };
+
+  const switchToLoginDialogHandler = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    onSignUpDialogOpenChange(false);
+    onLogInDialogOpenChange(true);
+  };
 
   return (
     <Dialog open={isSignUpDialogOpen} onOpenChange={onSignUpDialogOpenChange}>
@@ -106,7 +203,106 @@ const SignUpDialog = ({
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="grid gap-4">
+                  <div className="flex flex-col gap-2">
+                    <TButton
+                      onClick={handleGoogleSignUp}
+                      disabled={googleAuthLoading}
+                    >
+                      <GoogleLogo className="fill-white" />
+                      Sign up with Google
+                    </TButton>
+                  </div>
+                  <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                      Or continue with
+                    </span>
+                  </div>
+                  <div className="grid gap-2 text-sm">
+                    <div className="grid gap-1">
+                      <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                              <Input type="text" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <div className="grid gap-1">
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                              <Input type="text" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <div className="grid gap-1">
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                              <Input type="password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <div className="grid gap-1">
+                      <FormField
+                        control={form.control}
+                        name="confirmPassword"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Confirm Password</FormLabel>
+                            <FormControl>
+                              <Input type="password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <TButton
+                      type="submit"
+                      className="mt-2"
+                      disabled={emailAuthLoading}
+                    >
+                      Sign up
+                    </TButton>
+                  </div>
+                  <div className="text-center text-sm">
+                    Already have an account?{" "}
+                    <TButton
+                      variant="link"
+                      onClick={switchToLoginDialogHandler}
+                    >
+                      Log in
+                    </TButton>
+                  </div>
+                </div>
+              </form>
+            </Form>
+
+            {/* <form onSubmit={handleSubmit(onSubmit)}>
               <div className="grid gap-4">
                 <div className="flex flex-col gap-2">
                   <TButton
@@ -213,7 +409,7 @@ const SignUpDialog = ({
                   </TButton>
                 </div>
               </div>
-            </form>
+            </form> */}
           </CardContent>
         </Card>
       </DialogContent>
